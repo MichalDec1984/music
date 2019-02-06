dea8d4bbb48bf6f8e3430e88129c3edfcfef9731
Michał <michaldec84@gmail.com>
2019-02-06 14:43:49 +0100

    Updates

---
+++ precache-manifest.41e502e0160b6946797f3ab99a6f8707.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "cf4449c1884598b292630aa9f4007c19",
+    "url": "/music/static/media/roboto-condensed.light.cf4449c1.ttf"
+  },
+  {
+    "revision": "8262057119c963741b868e0d8dd68dd3",
+    "url": "/music/static/media/economica-bold.82620571.ttf"
+  },
+  {
+    "revision": "5604c427e1d539a6c107",
+    "url": "/music/static/js/runtime~main.5604c427.js"
+  },
+  {
+    "revision": "6203eaceced2a092b19f",
+    "url": "/music/static/js/main.6203eace.chunk.js"
+  },
+  {
+    "revision": "609c6d65a2e5b667027b",
+    "url": "/music/static/js/1.609c6d65.chunk.js"
+  },
+  {
+    "revision": "6203eaceced2a092b19f",
+    "url": "/music/static/css/main.e1dfa0a8.chunk.css"
+  },
+  {
+    "revision": "9f5b6b3e72c9c516b3dabc991a560f5c",
+    "url": "/music/index.html"
+  }
+];
